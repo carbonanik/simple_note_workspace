f2ea2df150a4b76355258eaabb40d1bc6e29c119
Anik <sheikanikbd@gmail.com>
2025-09-28 23:26:08 +0600

    feat(ui): notes page, note editor page added

--- apps/simple_note/lib/features/notes/presentation/pages/note_editor_page.dart
+++ apps/simple_note/lib/features/notes/presentation/pages/note_editor_page.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:simple_note/features/notes/presentation/pages/notes_page.dart';
 
+class NoteEditorPage extends StatelessWidget {
+  final NoteEntity note;
+  const NoteEditorPage({required this.note, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Note Editor'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const NotesPage()),
+              );
+            },
+            icon: const Icon(Icons.save),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          TextField(controller: TextEditingController(text: note.title)),
+          TextField(controller: TextEditingController(text: note.content)),
+        ],
+      ),
+    );
+  }
+}
--- apps/simple_note/lib/features/notes/presentation/pages/notes_page.dart
+++ apps/simple_note/lib/features/notes/presentation/pages/notes_page.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+class NoteEntity {
+  final String id;
+  final String title;
+  final String content;
+
+  NoteEntity({required this.id, required this.title, required this.content});
+}
+
+final notes = [
+  NoteEntity(id: '1', title: 'Note 1', content: 'Content 1'),
+  NoteEntity(id: '2', title: 'Note 2', content: 'Content 2'),
+  NoteEntity(id: '3', title: 'Note 3', content: 'Content 3'),
+];
+
+class NotesPage extends StatelessWidget {
+  const NotesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notes')),
+      body: GridView.builder(
+        itemCount: notes.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          final note = notes[index];
+          return Card(
+            child: ListTile(
+              title: Text(note.title),
+              subtitle: Text(note.content),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
